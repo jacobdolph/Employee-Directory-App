098645c94ad936f8646c6f86f43c56f3e2ff699b
Jacob Dolph <jacob.dolph702@gmail.com>
2020-06-14 21:48:31 -0600

    creating a database see for the employee directory database

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments(department_name, createdAt, updatedAt) VALUES('Executive','2020-01-01 10:10:10','2020-01-01 10:10:10');
+INSERT INTO departments(department_name, createdAt, updatedAt) VALUES('Administration','2020-01-01 10:10:10','2020-01-01 10:10:10');
+INSERT INTO departments(department_name, createdAt, updatedAt) VALUES('Finance','2020-01-01 10:10:10','2020-01-01 10:10:10');
+INSERT INTO departments(department_name, createdAt, updatedAt) VALUES('Sales','2020-01-01 10:10:10','2020-01-01 10:10:10');
+INSERT INTO departments(department_name, createdAt, updatedAt) VALUES('Marketing','2020-01-01 10:10:10','2020-01-01 10:10:10');
+INSERT INTO departments(department_name, createdAt, updatedAt) VALUES('HR','2020-01-01 10:10:10','2020-01-01 10:10:10');
+INSERT INTO departments(department_name, createdAt, updatedAt) VALUES('IT','2020-01-01 10:10:10','2020-01-01 10:10:10');
+
+
+
+INSERT INTO employee_roles(role_name, base_pay, createdAt, updatedAt, departmentId) VALUES('CEO',200000,'2020-01-01 10:10:10','2020-01-01 10:10:10', 15);
