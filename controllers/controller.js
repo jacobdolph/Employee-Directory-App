d048ce363f5d6f8082383a0386bb674e8ca81131
Jacob Dolph <jacob.dolph702@gmail.com>
2020-06-17 20:53:04 -0600

    created post api routes for employees

--- controllers/controller.js
+++ controllers/controller.js
@@ -20,4 +20,19 @@ module.exports = function (app) {
             res.json(dbRole)
         })
     })
+    app.post('/api/employee', function (req, res) {
+        db.employee.create(req.body).then(function (dbEmployee) {
+            res.json(dbEmployee)
+        })
+    })
+    app.get('/api/employee/:id', function (req, res) {
+        db.employee.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: { all: true, nested: true }
+        }).then(function (dbRole) {
+            res.json(dbRole)
+        })
+    })
 }
