29c67393807e91a0d3f5d791c205ce528e230f09
Jacob Dolph <jacob.dolph702@gmail.com>
2020-06-17 20:24:59 -0600

    created nested get queries for employees

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,7 +7,16 @@ module.exports = function (app) {
         })
     })
     app.get('/api/role', function (req, res) {
-        db.department.findAll({ raw: true }).then(function (dbRole) {
+        db.employee_role.findAll({
+            include: [db.department]
+        }).then(function (dbRole) {
+            res.json(dbRole)
+        })
+    })
+    app.get('/api/employee', function (req, res) {
+        db.employee.findAll({
+            include: { all: true, nested: true }
+        }).then(function (dbRole) {
             res.json(dbRole)
         })
     })
