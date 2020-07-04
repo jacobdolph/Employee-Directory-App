c41c4936574db0fd11c35f076d23f48a8f59dd53
Jacob Dolph <jacob.dolph702@gmail.com>
2020-07-03 20:53:01 -0600

    adding a folder for assets images and icons

---
+++ client/src/components/SideNav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+function SideNav() {
+
+    return (
+        <div className='h-100 d-inline-block w-100 p-3 bg-dark text-light' style={{ marginLeft: '-15px' }}>
+            <p>hello world</p>
+        </div>
+    )
+}
+
+export default SideNav;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Col, Row, Container } from '../components/Grid';
 import EmployeeList from '../components/EmployeeList';
+import SideNav from '../components/SideNav';
 
 function Home() {
     return (
         <Container fluid>
             <Row>
-                <Col size='md-2'></Col>
+                <Col size='md-2'>
+                    <SideNav></SideNav>
+                </Col>
                 <Col size='md-8'>
                     <EmployeeList />
                 </Col>
